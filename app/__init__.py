ed36bbbf9287611017f25ec250f4239e203d0a52
abdulghanialjuhi <gfyjd@hotmail.com>
2022-03-26 23:18:54 +0800

    up

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask, send_from_directory
 app = Flask(__name__, static_folder='../front-end/build', static_url_path='')
 
 
-
 @app.errorhandler(404)
 def serve(e):
     return send_from_directory(app.static_folder, 'index.html')
